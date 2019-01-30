1e370ce0b9bb19bd29d17fd21e11294ac4909ba9
josepheastman <josephryan96@gmail.com>
2019-01-30 12:43:03 -0800

    Dashboard tests WIP

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
 // Test away
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "./Dashboard.js";
+
+describe("<Dashboard />", () => {
+  it("renders close, open and lock, unlock status", () => {
+    const { getByText, debug } = render(<Dashboard />);
+
+    debug();
+
+    getByText(/lock/i);
+    getByText(/close/i);
+    getByText(/open/i);
+    getByText(/unlocked/i);
+  });
+
+  it("unmounts component after each test", () => {
+    console.log(document.body.outerHTML);
+  });
+});
