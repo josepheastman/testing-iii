180977e18e9d4fffa15c16dc39026d8b226fb760
josepheastman <josephryan96@gmail.com>
2019-01-30 14:30:13 -0800

    Display tests WIP

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,33 @@
 // Test away!
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+import Display from "./Display.js";
+
+describe("<Display />", () => {
+  it("locked and closed are equal to true & have red-led class", () => {
+    const { getByTestId } = render(<Display locked={true} closed={true} />);
+
+    const closed = getByTestId("closed");
+    const locked = getByTestId("locked");
+
+    expect({ closed, locked }).toHaveClass("red-led");
+
+    expect(locked).toHaveTextContent(/Locked/i);
+    expect(closed).toHaveTextContent(/Closed/i);
+  });
+
+  it("open and unlocked are equal to true & have green-led class", () => {
+    const { getByTestId } = render(<Display locked={false} closed={false} />);
+    const open = getByTestId("open");
+    const unlocked = getByTestId("unlocked");
+
+    expect({ open, unlocked }).toHaveClass("green-led");
+  });
+
+  it("unmounts component after each test", () => {
+    console.log(document.body.outerHTML);
+  });
+});
