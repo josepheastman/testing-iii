0453f8157ce4d64d46c3b82a9ad324faff909bcd
josepheastman <josephryan96@gmail.com>
2019-01-30 15:45:25 -0800

    Added tests to Display spec

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -27,6 +27,26 @@ describe("<Display />", () => {
     expect(lockedDisplay).toHaveTextContent(/unlocked/i);
   });
 
+  it("should use red-led class when closed", () => {
+    const { getByText } = render(<Display closed={true} />);
+
+    const closedDisplay = getByText(/closed/i);
+
+    expect(closedDisplay).toHaveClass("red-led");
+
+    expect(closedDisplay).toHaveTextContent(/closed/i);
+  });
+
+  it("should use green-led class when open", () => {
+    const { getByText } = render(<Display closed={false} />);
+
+    const closedDisplay = getByText(/open/i);
+
+    expect(closedDisplay).toHaveClass("green-led");
+
+    expect(closedDisplay).toHaveTextContent(/open/i);
+  });
+
   it("unmounts component after each test", () => {
     console.log(document.body.outerHTML);
   });
